414a56726af8b8b275cd987332f93cc28f284165
Alex <rollinglex@gmail.com>
2018-12-15 04:38:49 -0600

    add NavBar and Logo

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../Helpers/Logo';
+
+function NavBar() {
+  return (
+    <>
+      <Logo />
+      <div>Login</div>
+    </>
+  );
+}
+
+export default NavBar;
