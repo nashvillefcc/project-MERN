71fcf3c10d2ebeea2c0ccb8384cde3c2508d5a32
Alex <rollinglex@gmail.com>
2018-12-15 03:09:30 -0600

    change to single quotes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import GlobalStyles from "../styles/globalStyles";
+import React, { Component } from 'react';
+import GlobalStyles from '../styles/globalStyles';
 class App extends Component {
   render() {
     return (
--- client/src/styles/variables.js
+++ client/src/styles/variables.js
@@ -1,9 +1,9 @@
 export const colors = {
-  blackPrimary: "#1d1d1d",
-  whitePrimary: "#f9f9f9",
-  grayPrimary: "#6a6a6a",
-  backgroundPrimary: "#f6f9ff",
-  backgroundSecondary: "#fff",
-  likedPrimary: "#ff4545",
-  likedSecondary: "rgba(255, 215, 222, 0.8)"
+  blackPrimary: '#1d1d1d',
+  whitePrimary: '#f9f9f9',
+  grayPrimary: '#6a6a6a',
+  backgroundPrimary: '#f6f9ff',
+  backgroundSecondary: '#fff',
+  likedPrimary: '#ff4545',
+  likedSecondary: 'rgba(255, 215, 222, 0.8)'
 };
