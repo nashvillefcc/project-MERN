3d99bb376bdd028e6b5ef001a4198c9fad65db26
Alex <rollinglex@gmail.com>
2018-12-17 03:57:25 -0600

    heading component font-size dynamic

--- client/src/components/Helpers/Heading/HeadingStyle.js
+++ client/src/components/Helpers/Heading/HeadingStyle.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export default styled.div`
   display: block;
   h1 {
+    font-size: 3vmin;
     color: ${props => (props.color ? props.color : '#000')};
   }
 `;
