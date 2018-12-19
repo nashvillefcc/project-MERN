1e99453acb4ff334d73a8b427f7ceec85249d4bc
Alex <rollinglex@gmail.com>
2018-12-19 01:20:27 -0600

    change event to class component

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Wrapper from './EventsStyle';
@@ -6,20 +6,29 @@ import { colors } from '../../styles/variables';
 import Heading from '../Helpers/Heading';
 import EventsSearchBarContainer from '../EventsSearchBar';
 import EventsList from '../EventsList';
-const Events = props => {
-  return (
-    <Wrapper>
-      <div>
-        <Heading color={colors.thirdColor}>
-          Find your kindred spirts from Meetup.com
-        </Heading>
-        <EventsSearchBarContainer />
-        <div>sort</div>
-        <EventsList />
-      </div>
-    </Wrapper>
-  );
-};
+
+class Events extends Component {
+  state = { events: [] };
+
+  componentDidMount() {
+    //get events data
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <div>
+          <Heading color={colors.thirdColor}>
+            Find your kindred spirts from Meetup.com
+          </Heading>
+          <EventsSearchBarContainer />
+          <div>sort</div>
+          <EventsList />
+        </div>
+      </Wrapper>
+    );
+  }
+}
 
 Events.propTypes = {};
 
