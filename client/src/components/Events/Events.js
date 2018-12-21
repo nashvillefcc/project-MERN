32de36ffcc9339ba1cfafbbf60fcb38c3ed84a7a
Alex <rollinglex@gmail.com>
2018-12-21 02:35:43 -0600

    setup Modals

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -2,22 +2,67 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Wrapper from './EventsStyle';
-
+import { breakPoints } from '../../styles/variables';
 import EventsResult from '../EventsResult';
 import EventView from '../EventView/EventView';
+import ModalContainer from '../Helpers/Modal';
 class Events extends Component {
-  state = { events: [] };
+  state = {
+    events: [],
+    showModal: true,
+    selectedEvent: {},
+    windowWidth: 'mid-size'
+  };
 
+  toggleModal = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      showModal: !prevState.showModal
+    }));
+  };
+
+  widthBreakpointHandler() {
+    //sets state.windowWidth if width of window moves over breakpoint
+    if (this.state.windowWidth === 'mid-size') {
+      if (window.innerWidth > breakPoints.mid) {
+        this.setState(prevState => {
+          return { ...prevState, windowWidth: 'full-size' };
+        });
+        return;
+      }
+    } else if (this.state.windowWidth === 'full-size') {
+      if (window.innerWidth < breakPoints.mid) {
+        this.setState(prevState => {
+          return { ...prevState, windowWidth: 'mid-size' };
+        });
+        return;
+      }
+    }
+  }
   componentDidMount() {
     //get events data
+    //set width of window to state
+    this.widthBreakpointHandler();
+    window.addEventListener('resize', this.widthBreakpointHandler.bind(this));
   }
-
+  componentWillUnmount() {
+    window.removeEventListener(
+      'resize',
+      this.widthBreakpointHandler.bind(this)
+    );
+  }
   render() {
-    const { events } = this.state;
+    const { events, showModal, selectedEvent, windowWidth } = this.state;
     return (
       <Wrapper>
         <EventsResult events={events} />
-        <EventView />
+        {windowWidth === 'full-size' ? (
+          <EventView eventData={selectedEvent || null} />
+        ) : (
+          <ModalContainer showModal={showModal} toggleModal={this.toggleModal}>
+            <EventView eventData={selectedEvent || null} />
+          </ModalContainer>
+        )}
       </Wrapper>
     );
   }
---
+++ client/src/components/Helpers/Modal/style/ModalContainerStyle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+  display: flex;
+  position: absolute;
+  height: 100%;
+  background-color: #a5a5a5bf;
+  width: 100%;
+`;
+
+export default ModalContainer;
