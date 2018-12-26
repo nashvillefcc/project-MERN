07fbfdad58dcdf6bda3547d3bfc181ffd7d62a64
Alex <rollinglex@gmail.com>
2018-12-26 02:05:08 -0600

    setup EventAttendee component

---
+++ client/src/components/EventAttendee/EventAttendee.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Wrapper from './EventAttendeeStyle';
+const EventAttendee = props => {
+  return <Wrapper>Attendees</Wrapper>;
+};
+
+EventAttendee.propTypes = {};
+
+export default EventAttendee;
