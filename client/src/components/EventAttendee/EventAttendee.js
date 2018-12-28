f5f868d5829567ad6968d4af26565adb98fc8882
Alex <rollinglex@gmail.com>
2018-12-28 03:03:51 -0600

    styled attendee cards

--- client/src/components/EventAttendee/EventAttendee.js
+++ client/src/components/EventAttendee/EventAttendee.js
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types';
 
 import Wrapper from './EventAttendeeStyle';
 const EventAttendee = ({ member }) => {
-  console.log('member', member);
+  console.log('member', member.name);
   const connections = member.connections ? member.connections : 0;
+  const name = member.name.split(' ');
   return (
     <Wrapper>
       <div>
-        <p>{member.name}</p>
+        <img
+          src={`https://source.unsplash.com/60x60/?face,${member.name}`}
+          alt="user profile"
+        />
+      </div>
+      <div>
+        <p>{name[0]}</p>
+        <p>{name[1]}</p>
         <p>{connections} shared groups</p>
       </div>
     </Wrapper>
--- client/src/components/EventAttendee/EventAttendeeStyle.js
+++ client/src/components/EventAttendee/EventAttendeeStyle.js
@@ -3,7 +3,27 @@ import { colors } from '../../styles/variables';
 
 export default styled.div`
   display: flex;
-  background-color: pink;
   flex-direction: column;
+  border: 1px solid gray;
+  border-radius: 3px;
+  min-height: 10rem;
+  padding: 0.5rem 0;
+  margin: 0.5rem 0;
   width: 17.5%;
+  box-shadow: 1px 1px rgba(54, 54, 54, 0.1);
+  > div {
+    margin: 0.5rem;
+    text-align: center;
+    img {
+      border-radius: 50%;
+    }
+    p {
+      font-size: 0.7rem;
+      margin: 0.3rem 0;
+    }
+    p:nth-child(-n + 2) {
+      font-weight: bold;
+      font-size: 0.9rem;
+    }
+  }
 `;
