c45be085b1d44f2a326c7d29e8807410ded968a1
Warpfox <tylermwatts@gmail.com>
2019-07-07 10:33:07 -0500

    wip setting up auth requests

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
+app.get('/login', (req, res) => {
+  res.redirect(
+    `https://secure.meetup.com/oauth2/authorize?client_id=${
+      process.env.CLIENT_ID
+    }&response_type=token&redirect_uri=${process.env.REDIRECT_URI}`
+  );
 });
 
-const port = 5000;
+app.get('/login/auth_redirect', async (req, res) => {
+  const access_token = req.body.access_token;
+  const groups = await fetch('https://api.meetup.com/self/groups', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + access_token,
+    },
+  }).then(response => response.json());
+  // may not need the whole array client-side... possibly just names and ids of groups?
+  res.send(groups);
+});
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(process.env.PORT || 3001, () => `Server running on port ${port}`);
