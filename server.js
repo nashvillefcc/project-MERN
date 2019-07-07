f23cce22bdb822318e3717ce5f62a61b8c76da2b
Warpfox <tylermwatts@gmail.com>
2019-07-07 12:53:57 -0500

    I think I did that right

--- server.js
+++ server.js
@@ -11,20 +11,38 @@ app.get('/login', (req, res) => {
   res.redirect(
     `https://secure.meetup.com/oauth2/authorize?client_id=${
       process.env.CLIENT_ID
-    }&response_type=token&redirect_uri=${process.env.REDIRECT_URI}`
+    }&response_type=code&redirect_uri=${process.env.REDIRECT_URI}`
   );
 });
 
 app.get('/login/auth_redirect', async (req, res) => {
-  const access_token = req.body.access_token;
-  const groups = await fetch('https://api.meetup.com/self/groups', {
-    method: 'GET',
+  const auth_code = req.body.code;
+  const groupArray = await fetch('https://secure.meetup.com/oauth2/access', {
+    method: 'POST',
     headers: {
-      Authorization: 'Bearer ' + access_token,
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-  }).then(response => response.json());
-  // may not need the whole array client-side... possibly just names and ids of groups?
-  res.send(groups);
+    body: {
+      client_id: process.env.CLIENT_ID,
+      grand_type: 'authorization_code',
+      redirect_uri: process.env.REDIRECT_URI,
+      code: auth_code,
+    },
+  })
+    .then(response => response.json())
+    .then(async data => {
+      return await fetch('https://api.meetup.com/self/groups', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + data.access_token,
+        },
+      })
+        .then(response => response.json())
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+  // may not need the whole object for each group in the array on the client-side... possibly just names and ids of groups?
+  res.send(groupArray).redirect('/');
 });
 
 app.listen(process.env.PORT || 3001, () => `Server running on port ${port}`);
