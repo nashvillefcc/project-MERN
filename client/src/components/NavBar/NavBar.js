69100aa1d6954fe6d573ba1aceef4fe41117cd1a
Warpfox <tylermwatts@gmail.com>
2019-07-08 09:04:27 -0500

    browser redirect on login

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Logo from '../Helpers/Logo';
-
 import Wrapper from './NavBarStyle';
+
 function NavBar() {
+  const handleLogin = async () => {
+    const response = await window
+      .fetch('/login')
+      .then(res => res.json())
+      .catch(err => console.log(err));
+    if (response.status === 301) {
+      window.location = response.redirect;
+    }
+  };
   return (
     <Wrapper>
       <div>
         <Logo />
-        <span>Login</span>
+        <span>
+          <button onClick={handleLogin}>Login</button>
+        </span>
       </div>
     </Wrapper>
   );
--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 const fetch = require('node-fetch');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get('/login', (req, res) => {
-  res.redirect(
-    `https://secure.meetup.com/oauth2/authorize?client_id=${
+  res.json({
+    status: 301,
+    redirect: `https://secure.meetup.com/oauth2/authorize?client_id=${
       process.env.CLIENT_ID
-    }&response_type=code&redirect_uri=${process.env.REDIRECT_URI}`
-  );
+    }&response_type=code&redirect_uri=${process.env.REDIRECT_URI}`,
+  });
 });
 
 app.get('/login/auth_redirect', async (req, res) => {
   const auth_code = req.query.code;
-  const groupArray = await fetch(`https://secure.meetup.com/oauth2/access`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      grant_type: 'authorization_code',
-      code: auth_code,
-    }),
-  })
+  const groupArray = await fetch(
+    `https://secure.meetup.com/oauth2/access?client_id=${
+      process.env.CLIENT_ID
+    }&client_secret=${
+      process.env.CLIENT_SECRET
+    }&grant_type=authorization_code&redirect_uri=${
+      process.env.REDIRECT_URI
+    }&code=${auth_code}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }
+  )
     .then(response => response.json())
     .then(async data => {
       return await fetch('https://api.meetup.com/self/groups', {
